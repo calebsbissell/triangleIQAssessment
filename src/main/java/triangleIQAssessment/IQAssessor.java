17fe425a6673330bb881276e34cc7f1b5db9686c
Caleb Bissell <calebsbissell@gmail.com>
2017-07-31 22:48:49 -0400

    Added some space.

--- src/main/java/triangleIQAssessment/IQAssessor.java
+++ src/main/java/triangleIQAssessment/IQAssessor.java
@@ -4,6 +4,7 @@ public class IQAssessor {
 
 	public static void main(String[] args) {
 		System.out.println("An 'X' indicates a peg and an 'O' indicates an empty space:");
+		System.out.println();
 		System.out.println("    O    ");
 		System.out.println("   X X   ");
 		System.out.println("  X X X  ");
