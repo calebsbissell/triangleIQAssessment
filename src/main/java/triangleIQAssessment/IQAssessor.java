63ab3e8776cc8c8b3d07760b1c68750b15941d46
Caleb Bissell <calebsbissell@gmail.com>
2017-07-31 22:09:43 -0400

    Created the board.

---
+++ src/main/java/triangleIQAssessment/IQAssessor.java
@@ -0,0 +1,14 @@
+package triangleIQAssessment;
+
+public class IQAssessor {
+
+	public static void main(String[] args) {
+		System.out.println("An 'X' indicates a peg and an 'O' indicates an empty space:");
+		System.out.println("    O    ");
+		System.out.println("   X X   ");
+		System.out.println("  X X X  ");
+		System.out.println(" X X X X ");
+		System.out.println("X X X X X");
+	}
+
+}
