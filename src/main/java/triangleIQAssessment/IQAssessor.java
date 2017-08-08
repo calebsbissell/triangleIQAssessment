0aeabffa67ead777962c1575e5495a89608aa6ac
Caleb Bissell <calebsbissell@gmail.com>
2017-08-07 20:04:41 -0400

    Created the changeable board.

--- src/main/java/triangleIQAssessment/IQAssessor.java
+++ src/main/java/triangleIQAssessment/IQAssessor.java
@@ -3,13 +3,18 @@ package triangleIQAssessment;
 public class IQAssessor {
 
 	public static void main(String[] args) {
+		char[] boardPosition = new char[15];
+		boardPosition[0] = 'O';
+		for (int i=1; i<15; i++) {
+			boardPosition[i] = 'X';
+		}
 		System.out.println("An 'X' indicates a peg and an 'O' indicates an empty space:");
 		System.out.println();
-		System.out.println("    O    ");
-		System.out.println("   X X   ");
-		System.out.println("  X X X  ");
-		System.out.println(" X X X X ");
-		System.out.println("X X X X X");
+		System.out.println("    "+boardPosition[0]+"    ");
+		System.out.println("   "+boardPosition[1]+" "+boardPosition[2]+"   ");
+		System.out.println("  "+boardPosition[3]+" "+boardPosition[4]+" "+boardPosition[5]+"  ");
+		System.out.println(" "+boardPosition[0]+" "+boardPosition[7]+" "+boardPosition[8]+" "+boardPosition[9]+" ");
+		System.out.println(boardPosition[10]+" "+boardPosition[11]+" "+boardPosition[12]+" "+boardPosition[13]+" "+boardPosition[14]);
 	}
 
 }
