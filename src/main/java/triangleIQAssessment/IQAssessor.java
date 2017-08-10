6d63a99116eac387bd082991c6666920645ff3c7
Caleb Bissell <calebsbissell@gmail.com>
2017-08-10 18:19:39 -0400

    Started making it take only legal moves.

--- src/main/java/triangleIQAssessment/IQAssessor.java
+++ src/main/java/triangleIQAssessment/IQAssessor.java
@@ -4,12 +4,19 @@ public class IQAssessor {
 
 	public static void main(String[] args) {
 		Board board = new Board();
+		
 		board.printBoard();
+		
 		board.removePeg(15);
+		
 		board.printBoard();
+		
 		board.removePeg(8);
+		
 		board.printBoard();
+		
 		board.addPeg(15);
+		
 		board.printBoard();
 	}
 
---
+++ src/main/java/triangleIQAssessment/Moves.java
@@ -0,0 +1,13 @@
+package triangleIQAssessment;
+
+public class Moves {
+	private int startingPoint, endingPoint, middlePoint;
+
+	public Moves(int startingPoint, int middlePoint, int endingPoint) {
+		
+		this.startingPoint = startingPoint;
+		this.endingPoint = endingPoint;
+		this.middlePoint = middlePoint;
+	}
+
+}
