54b62cfd4403353e8b549173a4d0d6a73e1f9f50
Caleb Bissell <calebsbissell@gmail.com>
2017-08-10 19:26:54 -0400

    Added getters and setters to Moves.

--- src/main/java/triangleIQAssessment/Moves.java
+++ src/main/java/triangleIQAssessment/Moves.java
@@ -10,4 +10,28 @@ public class Moves {
 		this.middlePoint = middlePoint;
 	}
 
+	public int getStartingPoint() {
+		return startingPoint;
+	}
+
+	public void setStartingPoint(int startingPoint) {
+		this.startingPoint = startingPoint;
+	}
+
+	public int getEndingPoint() {
+		return endingPoint;
+	}
+
+	public void setEndingPoint(int endingPoint) {
+		this.endingPoint = endingPoint;
+	}
+
+	public int getMiddlePoint() {
+		return middlePoint;
+	}
+
+	public void setMiddlePoint(int middlePoint) {
+		this.middlePoint = middlePoint;
+	}
+
 }
